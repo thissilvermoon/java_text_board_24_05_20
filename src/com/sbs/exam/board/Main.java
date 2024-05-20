5f721fceea17d3eccd6c5168b10f5045d13336f2
geo747 <ckwoghfksek@naver.com>
2024-05-21 00:13:03 +0900

    시작 알림 메세지 출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -4,8 +4,10 @@ package com.sbs.exam.board;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("--텍스트 게시판 자바--");
+        System.out.println("프로그램 시작");
+        System.out.println("명령" );
+        System.out.println("프로그램 종료");
 
         }
     }
