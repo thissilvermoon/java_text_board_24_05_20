bc08a9d8ef2d34b3482c0a26920f36f8e540f70c
geo747 <ckwoghfksek@naver.com>
2024-05-21 00:39:32 +0900

    무한 루프 만들기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,13 +1,33 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--텍스트 게시판 자바--");
-        System.out.println("프로그램 시작");
-        System.out.println("명령" );
-        System.out.println("프로그램 종료");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("==텍스트 게시판 자바==");
+        System.out.println("★프로그램 시작★");
+
+
+        while (true) {
+            System.out.printf("명령 :");
+            String cmss = sc.nextLine();
+            System.out.printf("입력된 명령어 : %s\n", cmss);
+
+            if(cmss.equals("exit")) {
+                System.out.println("프로그램을 종료합니다");
+            }
+
+        }
+
+
+      //  System.out.println("프로그램 종료");
+
+      //  sc.close();
 
         }
     }
